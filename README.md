663fd87a209b0a21815a3ca45f254fb1e796a4f9
Emilybrown245 <emilybrown@Emilys-MacBook-Pro.local>
2024-11-29 11:09:55 +0000

    correcting my functions

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Northcoders News API
+NC News API
 
 Task 1 - 
 You will need to create two .env files in your repo: .env.test and .env.development. See /db/setup.sql for the database names. In each .env file add PGDATABASE=, with the correct database name for that environment. Ensure the .env files are .gitignored.
--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -69,22 +69,43 @@ describe("GET /api/articles/:article_id", () => {
       })
     })
   })
-  test("200: Responds with comment count for articles with article id", () => {
+  test("200: Responds with an article including comment_count when article has comments", () => {
     return request(app)
-    .get('/api/articles/1?comment_count=true')
+    .get('/api/articles/1')
     .expect(200)
-    .then((response) => {
-        expect(response.body.comment_count).toBe(11);
-    });
-  });
-  test("400: Sends an appropriate status and error message when given an comment query", () => {
-    return request(app)
-      .get('/api/articles/3/?comment_count=hi')
-      .expect(400)
+    .then(({ body }) => {
+      const { article } = body;
+        expect(article).toMatchObject({
+          article_id: 1,
+          title: expect.any(String),
+          topic: expect.any(String),
+          author: expect.any(String),
+          created_at: expect.any(String),
+          votes: expect.any(Number),
+          article_img_url: expect.any(String),
+          comment_count: 11
+        })
+      })
+    })
+    test("200: Responds with an article including comment_count when article has no comments", () => {
+      return request(app)
+      .get('/api/articles/2')
+      .expect(200)
       .then(({ body }) => {
-        expect(body.msg).toBe('Invalid comment count query');
-      });
-  });
+        const { article } = body;
+          expect(article).toMatchObject({
+            article_id: 2,
+            title: expect.any(String),
+            topic: expect.any(String),
+            author: expect.any(String),
+            created_at: expect.any(String),
+            votes: expect.any(Number),
+            article_img_url: expect.any(String),
+            comment_count: 0
+          })
+        })
+      })
+
   test("404: Responds with 'Article Doesn't Exist' for article id that is valid but does not exist", () => {
     return request(app)
     .get("/api/articles/2000")
